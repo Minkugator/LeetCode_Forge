5e36d480f21762372354e2949a8139e751097449
Minkugator <mayankvip2204@gmail.com>
2026-09-15 22:33:16 +0530

    Time: 183 ms (92.7%), Space: 10.3 MB (99.32%) Time: O(M * N * 3^L), Space: O(L)

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, string& word, int row, int col, int index){
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size()){ 
+        return false;
+        }
+        if(board[row][col] != word[index]){
+        return false;
+        }
+        if(index == word.size() - 1){ 
+        return true;    
+        }
+        char temp = board[row][col];
+        board[row][col] = '-';
+
+        bool found = dfs(board, word, row + 1, col , index + 1) || dfs(board, word, row - 1, col , index + 1) || dfs(board, word, row, col + 1, index + 1) || dfs(board, word, row, col - 1, index + 1);
+
+        board[row][col] = temp;
+
+        return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word[0]){
+                    if(dfs(board, word, i , j , 0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
