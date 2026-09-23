31cecacf7ae2e69719739510aec689d0d06751c5
Minkugator <mayankvip2204@gmail.com>
2026-09-23 22:31:40 +0530

    Time: 0 ms (100%), Space: 102.5 MB (38.48%) Time: O(n), Space: O(1)

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        int target = total - x;
+
+        // If we need to remove the entire array
+        if (target == 0) return n;
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (left <= right && sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+
+        return maxLen == -1 ? -1 : n - maxLen;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
